e3ef8ebff2cc5dc2dd2a2027cb8657528107f77c
Marcus Rodrigues <markynrodrisilva@gmail.com>
2022-06-06 10:32:36 -0300

    Installation and grammar errors fixed

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-version="1.0"
-echo "Installing runwin $version"
+version="2.0"
+echo "# Script developed by mavinsi"
+echo "# github.com/mavinsi/runwin"
+echo "# runwin version $version"
+echo ""
+echo "- Installing..."
+echo "- Copying runwin to /bin/ folder"
 sudo cp runwin /bin/runwin
-echo "runwin installed sucesfull"
+echo "- runwin $version was sucessfully installed!"
